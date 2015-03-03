6009d0246a4bf643d442bb67862688223024e255
Angeleah Daidone <angeleah@gmail.com>
2015-03-03 12:25:17 -0600

    adding gulp tasks for images uglify sass and browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
-var gulp   = require('gulp');
+var dest = './www';
+var src = './src';
+
+var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var karma = require('karma');
 
+var sass          = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
+var sourcemaps    = require('gulp-sourcemaps');
+var browserSync   = require('browser-sync');
 
-// tasks
+var uglify        = require('gulp-uglify');
+
+var changed       = require('gulp-changed');
+var imagemin      = require('gulp-imagemin');
+
 gulp.task('lint', function() {
-  return gulp
-    .src(['gulpfile.js', 'src/javascript/*.js', 'spec/*_spec.js'])
+  gulp
+    .src(['gulpfile.js', src + '/javascript/*.js', 'spec/*_spec.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
@@ -18,9 +29,54 @@ gulp.task('karma', function() {
   });
 });
 
-// default
-gulp.task('default', ['lint'], function() {
-  gulp.watch(['src/javascript/*.js', 'spec/*_spec.js'], function() {
-    gulp.run('lint', 'karma');
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: dest
+    }
   });
 });
+
+gulp.task('sass', function () {
+  gulp
+    .src(src + '/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer({ browsers: ['last 2 version']}))
+    .pipe(gulp.dest(dest + '/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('uglify', function () {
+  gulp
+    .src(src + '/javascript')
+    .pipe(uglify())
+    .pipe(gulp.dest(dest + '/js'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('images', function () {
+  gulp
+    .src(src + '/images/*')
+    .pipe(changed(dest + '/img'))
+    .pipe(imagemin())
+    .pipe(gulp.dest(dest + '/img'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+// default
+// gulp.task('default', function() {
+//   gulp.watch(['src/javascript/*.js', 'spec/*_spec.js'], function() {
+//     gulp.run('lint', 'karma');
+    // gulp.watch(['src/javascript/*.js', 'spec/*_spec.js'], ['lint', 'karma']);
+
+  // });
+// });
+
+// gulp.task('default', ['browserSync'], function() {
+//   gulp.watch(src + '/scss/**/*.scss', ['sass']);
+//   gulp.watch(config.images.src, ['images']);
+//   gulp.watch(config.markup.src, ['markup']);
+  // gulp.watch(['src/javascript/*.js', 'spec/*_spec.js'], ['lint', 'karma', browserSync.reload]);
+// });
