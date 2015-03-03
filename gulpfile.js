e8c4baa82d54f22d59275faa1c8232d0aaace491
Angeleah Daidone <angeleah@gmail.com>
2015-03-03 09:10:30 -0600

    add gulp task for karma

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
+var karma = require('karma');
+
 
 // tasks
 gulp.task('lint', function() {
   return gulp
-    .src(['gulpfile.js', 'www/js/*.js', 'www/spec/*.js'])
+    .src(['gulpfile.js', 'src/javascript/*.js', 'spec/*_spec.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-// default
+gulp.task('karma', function() {
+  karma.server.start({
+    configFile: process.cwd() + '/karma.conf.js',
+    singleRun: true
+  });
+});
 
+// default
 gulp.task('default', ['lint'], function() {
-  gulp.watch(['www/js/*.js', 'www/test/*.js'], function() {
-    gulp.run('lint');
+  gulp.watch(['src/javascript/*.js', 'spec/*_spec.js'], function() {
+    gulp.run('lint', 'karma');
   });
 });
